2b28e963df2800d3db3fa0abd9c5a819efefb671
Petter Daae <petter.daae@gmail.com>
2021-08-07 14:41:29 +0200

    Add cors middleware

--- internal/router.go
+++ internal/router.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,9 +11,28 @@ func (d *Dependencies) health(c *gin.Context) {
 	c.String(http.StatusOK, "Healthy")
 }
 
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", os.Getenv("ORIGIN"))
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
+}
+
 // SetupRoutes assigns functions to all the different routes
 func SetupRoutes(d *Dependencies) *gin.Engine {
 	r := gin.Default()
+
+	r.Use(corsMiddleware())
 
 	r.GET("/health", d.health)
 
