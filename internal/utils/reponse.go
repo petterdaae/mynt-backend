33fc88cda94b14329b8d23ecc9cd96aebe653ae9
Petter Daae <petter.daae@gmail.com>
2021-08-24 10:26:14 +0200

    Don't panic on error

--- internal/utils/reponse.go
+++ internal/utils/reponse.go
@@ -7,11 +7,15 @@ import (
 )
 
 // TODO : Is this actually necessary?
-func AbortWithError(c *gin.Context, code int, abortError error) {
+func AbortWithError(c *gin.Context, code int, abortError error) int {
 	err := c.AbortWithError(http.StatusInternalServerError, abortError)
 	if err != nil {
-		panic(err)
+		// This error is not actually an error, its just
+		// c.Error() returning the struct that gin uses
+		// to store the error internally.
+		return 1
 	}
+	return 0
 }
 
 func InternalServerError(c *gin.Context, err error) {
