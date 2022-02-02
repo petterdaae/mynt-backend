494451ec7f27a92eb7a93217242cc1db71fd1de8
Petter Daae <petter.daae@gmail.com>
2022-02-02 19:58:03 +0100

    Update main budget id

--- internal/routes/budgets/create.go
+++ internal/routes/budgets/create.go
@@ -9,8 +9,9 @@ import (
 )
 
 type CreateBudgetBody struct {
-	Name  string `json:"name"`
-	Color string `json:"color"`
+	Name         string `json:"name"`
+	Color        string `json:"color"`
+	IsMainBudget bool   `json:"isMainBudget"`
 }
 
 func Create(c *gin.Context) {
@@ -24,8 +25,9 @@ func Create(c *gin.Context) {
 		return
 	}
 
-	err = database.Exec(
-		"INSERT INTO budgets (user_id, name, color) VALUES ($1, $2, $3)",
+	var newBudgetID int64
+	row, err := database.QueryRow(
+		"INSERT INTO budgets (user_id, name, color) VALUES ($1, $2, $3) RETURNING id",
 		sub,
 		body.Name,
 		body.Color,
@@ -33,6 +35,23 @@ func Create(c *gin.Context) {
 	if err != nil {
 		utils.InternalServerError(c, fmt.Errorf("failed to insert new budget: %w", err))
 		return
+	}
+	err = row.Scan(&newBudgetID)
+	if err != nil {
+		utils.InternalServerError(c, fmt.Errorf("failed to scan new budget id: %w", err))
+		return
+	}
+
+	if body.IsMainBudget {
+		err = database.Exec(
+			"UPDATE users SET main_budget = $1 WHERE user_id = $2",
+			newBudgetID,
+			sub,
+		)
+		if err != nil {
+			utils.InternalServerError(c, fmt.Errorf("failed to update field main_budget in users: %w", err))
+			return
+		}
 	}
 
 	c.Status(http.StatusCreated)
