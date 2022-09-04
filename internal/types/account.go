d3592575e99207c4515e86ce167077aff88e1b30
Petter Daae <petter.daae@bekk.no>
2022-09-04 22:05:52 +0200

    Fix json property name

--- internal/types/account.go
+++ internal/types/account.go
@@ -2,7 +2,7 @@ package types
 
 type Account struct {
 	ID            string `json:"id"`
-	AccountNumber string `json:"account_number"`
+	AccountNumber string `json:"accountNumber"`
 	Name          string `json:"name"`
 	Available     int    `json:"available"`
 	Balance       int    `json:"balance"`
